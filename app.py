0e5a6019f88093691724532221fd1f4b24ad84d6
c-bata <contact@c-bata.link>
2015-09-14 03:45:15 +0900

    Return 'Hello World' by wsgiref

---
+++ app.py
@@ -0,0 +1,12 @@
+def app(env, start_response):
+    start_response('200 OK', [('Content-Type','text/html')])
+    return [b"Hello World"]
+
+if __name__ == '__main__':
+    try:
+        from wsgiref.simple_server import make_server
+        httpd = make_server('', 8080, app)
+        print('Serving on port 8080...')
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print('Goodbye.')
