fa1f8b7ce97111cc13c9ac5a2f097b9295c99d84
c-bata <contact@c-bata.link>
2015-09-17 23:52:54 +0900

    Implement routing

--- kobin/app.py
+++ kobin/app.py
@@ -1,7 +1,28 @@
+from kobin.routes import Router, Route
+
+
 class Kobin(object):
+    def __init__(self):
+        self.router = Router()
+
+    def add_route(self, route):
+        self.router.add(route.rule, route.method, route)
+
+    def route(self, path=None, method='GET', callback=None):
+        def decorator(callback_func):
+            route = Route(self, path, method, callback_func)
+            self.add_route(route)
+            return callback_func
+        return decorator(callback) if callback else decorator
+
+    def _handle(self, environ):
+        route, args = self.router.match(environ)
+        return route.call()
+
     def wsgi(self, environ, start_response):
+        out = self._handle(environ)
         start_response('200 OK', [('Content-Type', 'text/html')])
-        return [b'<b>Hello World from wsgi</b>']
+        return [out]
 
     def __call__(self, environ, start_response):
         """It is called when receive http request."""
--- kobin_example.py
+++ kobin_example.py
@@ -3,6 +3,10 @@ from kobin.app import Kobin
 app = Kobin()
 
 
+@app.route('/')
+def hello():
+    return b"Hello World"
+
 if __name__ == '__main__':
     try:
         from wsgiref.simple_server import make_server
