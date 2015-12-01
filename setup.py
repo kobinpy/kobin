ff2dcfe81807d4ba20d8c9c5bf77ba55b667678d
c-bata <contact@c-bata.link>
2015-12-01 16:01:33 +0900

    HOT FIX: Fix setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,45 @@
 import os
-from setuptools import setup
-
-__author__ = 'Masashi Shibata <contact@c-bata.link>'
-__version__ = '0.0.0'
+from setuptools import setup, find_packages
+from kobin import (
+    __version__,
+    __author__,
+    __author_email__,
+    __license__,
+)
 
 BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(BASE_PATH, 'README.rst')).read()
 CHANGES = open(os.path.join(BASE_PATH, 'CHANGES.rst')).read()
 
+__classifiers__ = (
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries',
+    'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+    'Topic :: Internet :: WWW/HTTP :: WSGI',
+    'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
+    'Topic :: Internet :: WWW/HTTP :: WSGI :: Server',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+)
+
 setup(
     name='kobin',
     version=__version__,
     author=__author__,
-    author_email='contact@c-bata.link',
+    author_email=__author_email__,
     url='https://github.com/c-bata/kobin',
     description='A very simple web application framework.',
     long_description=README + '\n\n' + CHANGES,
-    package=['kobin'],
+    classifiers=__classifiers__,
+    packages=find_packages(exclude=['test*']),
     install_requirements=[],
-    keywords=['web', 'framework', 'waf'],
-    license='MIT License',
+    keywords='web framework waf',
+    license=__license__,
     include_package_data=True,
     test_suite='tests',
 )
