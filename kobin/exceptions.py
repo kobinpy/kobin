e25541079f8b086d7b1967794a1f3c1f0c0af56a
Eduard Nikolenko <eduardnikolenko@yandex.ru>
2015-12-02 22:19:28 +0300

    New super() style
    super() in Python 3 is equivalent super(HTTPError, self) in Python 2

--- kobin/exceptions.py
+++ kobin/exceptions.py
@@ -2,4 +2,4 @@
 class HTTPError(Exception):
     def __init__(self, status, message, *args, **kwargs):
         print(status, message)
-        super(HTTPError, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
