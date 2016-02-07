c2d6ac7e281d67c35f22011e08808e3f9de2a73e
c-bata <contact@c-bata.link>
2016-02-07 14:30:17 +0900

    Update Request object

--- kobin/request.py
+++ kobin/request.py
@@ -1,15 +1,18 @@
+import threading
 from typing import Dict
 
 
 class Request(object):
+    """ A wrapper for WSGI environment dictionaries.
     """
 
-    """
+    __slots__ = ('environ', )
     #: Maximum size of memory buffer for :attr:`body` in bytes.
     MEMFILE_MAX = 102400
 
     def __init__(self, environ: Dict=None):
         self.environ = {} if environ is None else environ
+        self.environ['kobin.request'] = self
 
     def copy(self):
         """ Return a new :class:`Request` with a shallow :attr:`environ`
@@ -52,9 +55,6 @@ class Request(object):
         for key in todelete:
             self.environ.pop('kobin.request.' + key, None)
 
-    def __iter__(self):
-        return iter(self.environ)
-
     def __len__(self):
         return len(self.environ)
 
@@ -63,5 +63,34 @@ class Request(object):
 
     def __repr__(self):
         return '<{cls}: {method} {url}>'.format(
-            cls=self.__class__.__name__, method=self.method, url=self.url
+            cls=self.__class__.__name__, method=self.method, url=self.path
         )
+
+
+def _local_property():
+    ls = threading.local()
+
+    def fget(_):
+        try:
+            return ls.var
+        except AttributeError:
+            raise RuntimeError("Request context not initialized.")
+
+    def fset(_, value):
+        ls.var = value
+
+    def fdel(_):
+        del ls.var
+
+    return property(fget, fset, fdel, 'Thread-local property')
+
+
+class LocalRequest(Request):
+    """ A thread local subclass of :class:`Request`
+    """
+    bind = Request.__init__
+    environ = _local_property()
+
+
+request = LocalRequest()
+local = threading.local()
