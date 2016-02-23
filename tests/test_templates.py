82c1bab80869226287d4c2c288d04202bed24ea8
c-bata <contact@c-bata.link>
2016-02-23 15:20:09 +0900

    Add file loader tests

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -1,22 +1,44 @@
 import os
 from unittest import TestCase
-from kobin.templates import Jinja2Template
+from unittest.mock import patch
+
+from kobin.exceptions import HTTPError
+from kobin.templates import Jinja2Template, load_file
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 class LoadFileTests(TestCase):
-    def test_load_file(self):
-        pass
+    def setUp(self):
+        self.template_dirs = [os.path.join(BASE_DIR, 'templates')]
 
-    def test_raise_403_do_not_have_access_permission(self):
-        pass
+    def test_load_file(self):
+        actual = load_file('jinja2.html', self.template_dirs)
+        expected = os.path.join(self.template_dirs[0], 'jinja2.html')
+        self.assertEqual(actual, expected)
+
+    @patch('os.access')
+    def test_raise_403_do_not_have_access_permission(self, mock_os_access):
+        mock_os_access.return_value = False
+        input_args = {
+            'name': 'jinja2.html',
+            'directories': self.template_dirs,
+        }
+        self.assertRaises(HTTPError, load_file, **input_args)
 
     def test_raise_404_because_file_does_not_exist(self):
-        pass
+        input_args = {
+            'name': 'does_not_exist.html',
+            'directories': [os.path.join(BASE_DIR, 'templates')]
+        }
+        self.assertRaises(HTTPError, load_file, **input_args)
 
     def test_raise_404_because_it_is_not_file(self):
-        pass
+        input_args = {
+            'name': 'this_is_not_file.html',
+            'directories': [os.path.join(BASE_DIR, 'templates')]
+        }
+        self.assertRaises(HTTPError, load_file, **input_args)
 
 
 class Jinja2TemplateTests(TestCase):
