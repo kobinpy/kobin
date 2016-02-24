725a4b77838b83c7bd85b4a851e5949d01ed2b7c
c-bata <contact@c-bata.link>
2016-02-25 04:54:11 +0900

    Add tests for static file view

--- tests/test_static_files.py
+++ tests/test_static_files.py
@@ -6,10 +6,23 @@ from kobin import Kobin, response
 from kobin.exceptions import HTTPError
 
 
-class StaticFilesTests(TestCase):
+class StaticFileViewTests(TestCase):
     def setUp(self):
         self.app = Kobin()
 
+    def test_routing_to_static_file(self):
+        test_env = {'REQUEST_METHOD': 'GET', 'PATH_INFO': '/static/static.css'}
+        actual_target, _ = self.app.router.match(test_env)
+        self.assertEqual(actual_target.callback, static_file)
+
+    def test_urlvars(self):
+        test_env = {'REQUEST_METHOD': 'GET', 'PATH_INFO': '/static/static.css'}
+        _, url_vars = self.app.router.match(test_env)
+        actual_filename = url_vars['filename']
+        self.assertEqual(actual_filename, 'static.css')
+
+
+class StaticFilesTests(TestCase):
     def test_return_static_file(self):
         actual = static_file('static.css', STATIC_DIRS[0])
         expected = 'body { color: black; }\n'.encode('utf-8')
