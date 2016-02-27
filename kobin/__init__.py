f00d0f1d9fb5260cf177a266853b9962f0b32915
c-bata <contact@c-bata.link>
2016-02-27 19:36:48 +0900

    Update version settings

--- kobin/__init__.py
+++ kobin/__init__.py
@@ -1,3 +1,5 @@
 from .app import Kobin, Config, current_app, current_config
 from .environs import request, response
 from .templates import template, jinja2_template
+
+__version__ = '0.0.4-alpha.1'
