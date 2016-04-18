49695899e32fcf8276090a11e7d73c3b8d4f108b
c-bata <contact@c-bata.link>
2016-04-18 11:07:53 +0900

    HTTPError function make public

--- kobin/__init__.py
+++ kobin/__init__.py
@@ -1,3 +1,4 @@
 from .app import Kobin, Config, current_app, current_config
 from .environs import request, response
 from .templates import template, jinja2_template
+from .exceptions import HTTPError
