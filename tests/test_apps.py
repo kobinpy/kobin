1c9045b00f43f4abfb9c98e5360923a9c4523fc6
c-bata <contact@c-bata.link>
2016-12-02 01:49:25 +0900

    Add tests for 500 raised

--- tests/test_apps.py
+++ tests/test_apps.py
@@ -17,6 +17,11 @@ class KobinTests(TestCase):
             body = "type: {}, value: {}".format(type(typed_id), typed_id)
             return Response(body)
 
+        @self.app.route('/test/raise500')
+        def raise500(typed_id: int):
+            a = 1 / 0
+            return Response("Don't reach here")
+
     def test_route(self):
         test_env = {'REQUEST_METHOD': 'GET', 'PATH_INFO': '/'}
         response = self.app._handle(test_env)
@@ -43,6 +48,28 @@ class KobinTests(TestCase):
         response = self.app._handle(test_env)
         actual = response._status_code
         expected = 404
+        self.assertEqual(actual, expected)
+
+    def test_response_status_when_500_raised(self):
+        test_env = {'REQUEST_METHOD': 'GET', 'PATH_INFO': '/test/raise500'}
+        response = self.app._handle(test_env)
+        actual = response._status_code
+        expected = 500
+        self.assertEqual(actual, expected)
+
+    def test_response_body_when_500_raised_and_enable_debugging(self):
+        self.app.config['DEBUG'] = True
+        test_env = {'REQUEST_METHOD': 'GET', 'PATH_INFO': '/test/raise500'}
+        response = self.app._handle(test_env)
+        actual = response.body
+        expected = [b'Internal Server Error']
+        self.assertNotEqual(actual, expected)
+
+    def test_response_body_when_500_raised_and_disable_debugging(self):
+        test_env = {'REQUEST_METHOD': 'GET', 'PATH_INFO': '/test/raise500'}
+        response = self.app._handle(test_env)
+        actual = response.body
+        expected = [b'Internal Server Error']
         self.assertEqual(actual, expected)
 
     def test_handled_body_message_when_404_not_found(self):
