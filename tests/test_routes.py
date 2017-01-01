58005645e6ffe173c5c43a171a80bbca6b35b4bb
c-bata <contact@c-bata.link>
2017-01-01 09:19:00 +0900

    Add reverse routing tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -65,3 +65,32 @@ class RouterTests(TestCase):
         self.router.add('GET', '/tests/{name}', 'hoge', dummy_func)
         test_env = {'REQUEST_METHOD': 'GET', 'PATH_INFO': '/this_is_not_found'}
         self.assertRaises(HTTPError, self.router.match, test_env)
+
+
+class ReverseRoutingTests(TestCase):
+    def setUp(self):
+        self.router = Router()
+
+        def index() -> Response:
+            return Response('hello world')
+
+        def user_detail(user_id: int) -> Response:
+            return Response(f'hello user{user_id}')
+
+        self.router.add('GET', '/', 'top', index)
+        self.router.add('GET', '/users/{user_id}', 'user-detail', user_detail)
+
+    def test_reverse_route_without_url_vars(self):
+        actual = self.router.reverse('top')
+        expected = '/'
+        self.assertEqual(actual, expected)
+
+    def test_reverse_route_with_url_vars(self):
+        actual = self.router.reverse('user-detail', user_id=1)
+        expected = '/users/1'
+        self.assertEqual(actual, expected)
+
+    def test_reverse_not_match(self):
+        actual = self.router.reverse('foobar', foo=1)
+        expected = None
+        self.assertEqual(actual, expected)
