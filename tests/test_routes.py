121a1282eb5772520db6409cd7f15ee8252dbb88
c-bata <contact@c-bata.link>
2017-01-01 09:11:34 +0900

    Refactor routing tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,36 +1,37 @@
 from unittest import TestCase
 from kobin.routes import Route, Router
-from kobin.environs import HTTPError
+from kobin.environs import HTTPError, Response
 
 
 class RouteTests(TestCase):
     def test_call(self):
-        def dummy_func(num: int):
-            return num
+        def dummy_func(num: int) -> Response:
+            return Response(f'hello {num}')
         route = Route('/hoge/{num}', 'GET', 'hoge', dummy_func)
-        self.assertEqual(route.callback(**{'num': 1}), 1)
+        actual_response = route.callback(num=1)
+        self.assertEqual(actual_response.body, [b'hello 1'])
 
     def test_match_method(self):
-        def dummy_func(num: int):
-            return num
+        def dummy_func(num: int) -> Response:
+            return Response(f'hello {num}')
         route = Route('/hoge/{num}', 'GET', 'hoge', dummy_func)
         self.assertTrue(route._match_method('GET'))
 
     def test_match_path(self):
-        def dummy_func(num: int):
-            return num
+        def dummy_func(num: int) -> Response:
+            return Response(f'hello {num}')
         route = Route('/hoge/{num}', 'GET', 'hoge', dummy_func)
         self.assertTrue(route._match_path('/hoge/1'))
 
     def test_match_path_with_slash(self):
-        def dummy_func(num: int):
-            return num
+        def dummy_func(num: int) -> Response:
+            return Response(f'hello {num}')
         route = Route('/hoge/{num}', 'GET', 'hoge', dummy_func)
         self.assertTrue(route._match_path('hoge/1/'))
 
     def test_match(self):
-        def dummy_func(num: int):
-            return num
+        def dummy_func(num: int) -> Response:
+            return Response(f'hello {num}')
         route = Route('/hoge/{num}', 'GET', 'hoge', dummy_func)
         self.assertTrue(route.match('GET', 'hoge/1/'))
 
@@ -40,8 +41,8 @@ class RouterTests(TestCase):
         self.router = Router()
 
     def test_match_dynamic_routes_with_casted_number(self):
-        def dummy_func(year: int) -> int:
-            return year
+        def dummy_func(year: int) -> Response:
+            return Response(f'hello {year}')
 
         self.router.add('GET', '/tests/{year}', 'hoge', dummy_func)
         test_env = {'REQUEST_METHOD': 'GET', 'PATH_INFO': '/tests/2015/'}
@@ -50,7 +51,7 @@ class RouterTests(TestCase):
 
     def test_match_dynamic_routes_with_string(self):
         def dummy_func(name):
-            return name
+            return Response(f'hello {name}')
 
         self.router.add('GET', '/tests/{name}', 'hoge', dummy_func)
         test_env = {'REQUEST_METHOD': 'GET', 'PATH_INFO': '/tests/kobin/'}
@@ -59,7 +60,7 @@ class RouterTests(TestCase):
 
     def test_404_not_found(self):
         def dummy_func(name):
-            return name
+            return Response(f'hello {name}')
 
         self.router.add('GET', '/tests/{name}', 'hoge', dummy_func)
         test_env = {'REQUEST_METHOD': 'GET', 'PATH_INFO': '/this_is_not_found'}
