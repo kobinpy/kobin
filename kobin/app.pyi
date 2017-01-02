860b96d9a77a901ea03ae26278e4467fe4fe6b56
c-bata <contact@c-bata.link>
2017-01-03 02:03:01 +0900

    Fix mypy

--- kobin/app.pyi
+++ kobin/app.pyi
@@ -1,4 +1,4 @@
-from jinja2 import Environment
+from jinja2 import Environment  # type: ignore
 from typing import Callable, Dict, List, Tuple, Iterable, TypeVar, Any
 from types import ModuleType
 
